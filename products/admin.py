cc83a841a5fcf786f7acea7233f0d7ccbcfcd221
Felipe <felipe.dev0809@gmail.com>
2025-01-21 17:22:24 -0300

    outlfows crud

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class PoductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'serie_number',)
+    search_fields = ('title',)
+    
+
+admin.site.register(models.Products, PoductAdmin)
