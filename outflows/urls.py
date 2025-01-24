a29676f4493c867860271c7adcf4e2ab4da9fcd0
Felipe <felipe.dev0809@gmail.com>
2025-01-24 00:47:29 -0300

    products crud and filters

--- outflows/urls.py
+++ outflows/urls.py
@@ -4,5 +4,5 @@ from .import views
 urlpatterns = [
     path('outflows/list/', views.OutflowListView.as_view(), name='outflow_list'),
     path('outflows/create/', views.OutflowCreateView.as_view(), name='outflow_create'),
-    path('outflows/<int:pk>/detail/', views.OutflowDetailView.as_view(), name='outflows_detail'),
+    path('outflows/<int:pk>/detail/', views.OutflowDetailView.as_view(), name='outflow_detail'),
 ]
